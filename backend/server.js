46ae2e9b9c8aec157b6917de2e9e0b8aece89015
Prince188 <120271895+Prince188@users.noreply.github.com>
2024-05-02 22:03:56 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mern-chat-2-ewhs.onrender.com",
     // credentials: true,
   },
 });
